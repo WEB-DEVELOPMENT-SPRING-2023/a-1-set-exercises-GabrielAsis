99b986da625329f4687c9ea91bb1f98dafacc672
GabrielAsis <asisgab24@gmail.com>
2023-04-11 13:51:20 +0400

    comments

--- Chapter-6-Javascript/Exercises/Petrol-calcu.js
+++ Chapter-6-Javascript/Exercises/Petrol-calcu.js
@@ -1,15 +1,15 @@
-function calc(){
-    var liters=
-    document.getElementById('liters').value;
+function calc(){ // a functino name calc(). will be activated by the button on the html page
+    var liters= //setting a variable named 'liters'
+    document.getElementById('liters').value; //this will be the value of the variable above and gathers data from the input using ids
 
-    var cost=
-    document.getElementById('cost').value;
+    var cost=//setting a variable named 'cost'
+    document.getElementById('cost').value; //this will be the value of the variable above and gathers data from the input using ids
 
-    let result=
-    liters*cost;
+    let result= //setting a new variable named 'result'
+    liters*cost //this will use the 2 previous varibles and multiply them giving the value of the variable 'result'
 
-    document.getElementById('total').innerHTML="Your total cost is: ";
+    document.getElementById('total').innerHTML="Your total cost is: "; //prints out the desired sentence onto the html page
    
-    document.getElementById('result').innerHTML= + result;
+    document.getElementById('result').innerHTML= + result; //prints out the result variable onto the html page
 
 }
